568a6d3862bbc61e0f5d2042bd8b97b083f0f699
Pieter De Wit <pieter@redshield.co>
2024-05-22 09:17:42 +1200

    Remove NOT NULL constraint for now

--- html/install.php
+++ html/install.php
@@ -24,14 +24,14 @@
 	// sql to create table
 	$sql = "CREATE TABLE IF NOT EXISTS `users` (
 			`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			`username` varchar(100) NOT NULL,
-			`serialnumber` double NOT NULL,
-			`gender` varchar(10) NOT NULL,
-			`email` varchar(50) NOT NULL,
-			`fingerprint_id` int(11) NOT NULL,
+			`username` varchar(100),
+			`serialnumber` double,
+			`gender` varchar(10),
+			`email` varchar(50),
+			`fingerprint_id` int(11),
 			`fingerprint_select` tinyint(1) NOT NULL DEFAULT '0',
-			`user_date` date NOT NULL,
-			`time_in` time NOT NULL,
+			`user_date` date,
+			`time_in` time,
 			`del_fingerid` tinyint(1) NOT NULL DEFAULT '0',
 			`add_fingerid` tinyint(1) NOT NULL DEFAULT '0'
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
